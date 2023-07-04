82b6e6d9ac6811b67572b0a8f28d08d5fb85162b
Grzegorz Prabucki <g_prabucki@wp.pl>
2023-07-04 19:38:10 +0200

    changes after the mentor's advice

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@
     const  welcome = () => {
         console.log("Witam serdecznie w Repozytorium 😊");
     }
-    
-    const onChangeBackgroundClick = () => {
+    const init = () => {
+
+        const onChangeBackgroundClick = () => {
         body.classList.toggle("dark");
         themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-    }
+         }
     
-    welcome();
+        welcome();
     
-    const button = document.querySelector(".button");
-    const body = document.querySelector(".body");
-    const themeName = document.querySelector(".theme");
+        const button = document.querySelector(".button");
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".theme");
     
-    button.addEventListener("click", onChangeBackgroundClick);
+        button.addEventListener("click", onChangeBackgroundClick);
+    };
+    init();
 }
